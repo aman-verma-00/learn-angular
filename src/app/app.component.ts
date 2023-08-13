568905ade489d2e898b44a79bb4670d5c97f1851
aman-verma-00 <vermacamt1233@gmail.com>
2023-08-13 11:38:38 +0530

    name generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'utility-app';
+  name = 'John Doe';
+  firstName:string='Aman';
+  lastName:string='Verma'
+
+  // onFirstNameChange(firstName:string){
+  //   this.firstName = firstName;
+  // }
+  // onLastNameChange(lastName:string){
+  //   this.lastName = lastName;
+  // }
 }
